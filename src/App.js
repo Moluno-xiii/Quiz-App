b53275f13bf7cfc2491504e89c484c0eb82a7dea
Moluno-xiii <molunoprogress@gmail.com>
2023-08-28 00:04:07 +0100

    modified first stage of app

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      (Items.map())
-    </div>
-  );
+      {Items.map((item, i) => (
+        <div className="container ">
+          <div className="question-container">
+            <h2>{item.question}</h2>
+            <p>option {i + 1}: {item.options.option1}</p>
+            <p>option {i + 1}: {item.options.option2}</p>
+            <p>option {i + 1}: {item.options.option3}</p>
+            <p className='click'>option {i + 1}: {item.options.option4}</p>
+          </div>
+        </div>
+     ))}
+     </div>
+     );
 }
 
 
@@ -14,10 +24,12 @@ function App() {
 
 const Items = [{
   question: 'How many days are in a year',
-  option1 : '365 days', 
+  // options : ['365 days', '360 days', '355 1/4 days', '330 days']
+  options : {  option1 : '365 days', 
   option2 : '360 days', 
   option3 : '355 1/4 days', 
-  option4 :'money' ,
+  option4 :'money' ,}
+
 }]
 
 
